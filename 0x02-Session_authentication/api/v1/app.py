1d9cb9fa8cf6048644f3b9614d46d16496c25920
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-06-02 01:14:05 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,27 +2,33 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.basic_auth import BasicAuth
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv("AUTH_TYPE") == "auth":
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif getenv("AUTH_TYPE") == "session_auth":
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
